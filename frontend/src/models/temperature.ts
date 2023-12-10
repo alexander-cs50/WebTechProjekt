2741400b7e0a7e20f28ad5221ec631918d4a3489
Jeremia Henkel <henkel.jeremia@gmail.com>
2023-12-10 17:07:01 +0100

    frontend api calls and proxy setup for dev purpose

---
+++ frontend/src/models/temperature.ts
@@ -0,0 +1,8 @@
+export interface Temperature {
+    _id: string,
+    sensor: string,
+    recordedtemperature: string,
+    createdAt: string,
+    updateAt: string,
+}
+
---
+++ frontend/src/network/dataset_api.ts
@@ -0,0 +1,13 @@
+import { Temperature } from "../models/temperature";
+
+
+export async function getTemperature(): Promise<Temperature> {
+    const response = await fetch("api/temps", { method: "GET"});
+    if ( response.ok ) {
+        return response.json();
+    } else {
+        const errorBody = await response.json();
+        const errorMessage = errorBody.error;
+        throw Error("Request failed with status: " + response.status + "message" + errorMessage);
+      }
+    }
